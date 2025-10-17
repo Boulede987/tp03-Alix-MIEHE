b722ee1c8134297bd4df7d91a4abe9bac1693e04
MIEHE Alix <alixmiehe2004@gmail.com>
2025-10-17 21:20:21 +0200

    mise en place d'une barre de navigatuion par routes, ajout d'un service

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterModule], // necessary for router naviguation
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
